92c8031bcaad6e3c2bc1e0b3c73394ae80d032f2
Ahmed Azzaoui <ahmed.azzaoui@gmail.com>
2013-11-02 15:05:27 +0000

    Updated web.js with readFile

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var str = '';
+  fs.readFileSync('./index.html', function(err, data) {
+    if (err) throw err;
+    str = data.toString('utf8');
+  }
   response.send('Hello World2!');
 });
 
