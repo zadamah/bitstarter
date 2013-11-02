fd216c63408f3847934141e944e100bd09fbeaf1
Ahmed Azzaoui <ahmed.azzaoui@gmail.com>
2013-11-02 15:23:29 +0000

    Added a forgotten paranthesis to readFileSync function

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   fs.readFileSync('./index.html', function(err, data) {
     if (err) throw err;
     message = data.toString('utf8');
-  }
+  });
   response.send(message);
 });
 
