02d4d692ce040d37a490f31bf87414057f983e4d
Ahmed Azzaoui <ahmed.azzaoui@gmail.com>
2013-11-09 10:46:05 +0000

    Changed listening port of web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlFile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var message = fs.readFileSync('./index.html').toString('utf8');
-  response.send(message);
+  var html = fs.readFileSync(htmlFile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
